6ce19a290c91c405997736b97f1231b407cad41f
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-05-21 14:16:01 -0300

    Cria teste deveSalvarUmFilmeValidoComSucesso em FilmeRepositoryTest

---
+++ src/test/java/com/adatech/IMDB/repository/FilmeRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.adatech.IMDB.repository;
+
+import com.adatech.IMDB.model.Filme;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+@SpringBootTest
+class FilmeRepositoryTest {
+    @Autowired
+    FilmeRepository filmeRepository;
+    Filme filme;
+    @BeforeEach
+    void setUp(){
+        filme = new Filme();
+
+    }
+
+    @Test
+    void deveSalvarUmFilmeValidoComSucesso(){
+
+        //Cenário
+        filme.setTitle("Mission: Impossible - The Final Reckoning");
+        filme.setYear("2025");
+        filme.setPlot("Our lives are the sum of our choices. Tom Cruise is Ethan Hunt in Mission: Impossible - The Final Reckoning.");
+        filme.setRuntime("169 min");
+        filme.setActors("Vanessa Kirby, Tom Cruise, Hayley Atwell");
+        filme.setGenre("Action, Adventure, Thriller");
+
+        //Execução
+        Filme filmeSalvo = filmeRepository.save(filme);
+
+        //Validação
+        Assertions.assertNotNull(filmeSalvo);
+        Assertions.assertNotNull(filmeSalvo.getId());
+        Optional<Filme> filmeOptional = filmeRepository.findById(filmeSalvo.getId());
+        Assertions.assertTrue(filmeOptional.isPresent());
+        Assertions.assertEquals(filmeSalvo.getTitle(), filmeOptional.get().getTitle());
+        Assertions.assertEquals(filmeSalvo.getYear(), filmeOptional.get().getYear());
+    }
+}
